3e188458ad24343db595908531869f9d23777e9a
Renato Lopes de Carvalho <64995545+R10nato@users.noreply.github.com>
2022-09-30 00:43:59 -0400

    Update SwInterno.py
    Incrementando SwInterno

--- SwInterno.py
+++ SwInterno.py
@@ -1,4 +1,15 @@
-def implementar():
-    print('Software Implementado')
-    return 'Software Implementado'
+from ServicosDeTi import ServicoDeSoftware
+
+
+class SwInterno(ServicoDeSoftware):
+    def __init__(self, linguagem, versao, nome):
+        super().__init__(versao, nome)
+        self.linguagem = linguagem
+
+    def implementar():
+        print('Software Implementado')
+        return 'Software Implementado'
+    
+    implementar()
+
 
